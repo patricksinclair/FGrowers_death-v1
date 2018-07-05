5e19b051bf35a37261c3ae0a873ea8a249173b9d
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2018-07-05 13:58:44 +0100

    method commit

--- src/FastGrowersMain.java
+++ src/FastGrowersMain.java
@@ -1,6 +1,8 @@
 public class FastGrowersMain {
     public static void main(String[] args){
 
-        FastGrowerFrame fgFrame = new FastGrowerFrame(); fgFrame.setVisible(true);
+        //FastGrowerFrame fgFrame = new FastGrowerFrame(); fgFrame.setVisible(true);
+        double specific_alpha = Math.log(11.5)/500.;
+        BioSystem.exponentialGradient_spatialAndGRateDistributions(specific_alpha);
     }
 }
